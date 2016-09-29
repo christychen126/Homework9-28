b58798faceec2345823621d391454c4fbbfca645
Christy Chen <christychen126@gmail.com>
2016-09-28 20:22:32 -0700

    Differenciate underpay and overpay customers

--- check_underpaid.py
+++ check_underpaid.py
@@ -6,12 +6,16 @@ def check_underpaid_customer(customer_order_data):
     for line in customer_order:
         line.rstrip()   # take out white spaces at the right
         word = line.split("|")   # split the line by "|"
+        
         customer_name = word[1]  # set customer_name
         customer_bought = float(word[2])  # set customer_melon
         customer_paid = float(word[3])  # set customer_paid  
         customer_expected_paid = melon_cost * customer_bought  # set customer_expected
-        if customer_paid != customer_expected_paid:
-            print customer_name, "paid %.2f, expected %.2f" %(customer_paid, customer_expected_paid) 
-            #if customer_paid != customer_expected print warning
+        
+        if customer_paid > customer_expected_paid:
+            print "%s overpaid. Paid: %.2f, Expected: %.2f." %(customer_name, customer_paid, customer_expected_paid) 
+        
+        elif customer_paid < customer_expected_paid:
+            print "%s underpaid. Paid: %.2f, Expected: %.2f." %(customer_name, customer_paid, customer_expected_paid)
     customer_order.close
 check_underpaid_customer("customer-orders.txt")
